1c086a31d06ac93c0ce47d3a0ccb3169e2021fa5
=Hargun Singh <=Hargun0360@gmail.com>
2021-11-02 10:40:47 +0530

    adding validation in forgotpass page

--- components/Forgotpassword/Forgotpass.jsx
+++ components/Forgotpassword/Forgotpass.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Navbar from '../Navbar/Navbar'
 import Image from '../Imagecompo/Image'
 import './Forgot.css'
@@ -14,15 +14,47 @@ function Forgot() {
     }
     const [allEntry, setallEntery] = useState([]);
     const history = useHistory();
-    const submitForm = async (event) => {
-
-        event.preventDefault();
-        const newEntry = { ...user }
+    const [userError,setUserError]=useState({});
+    const [isSubmit,setIsSubmit] = useState(false);
+    useEffect(()=>{
+        if(Object.keys(userError).length===0 && isSubmit){
+            const newEntry = { ...user }
         setallEntery([...allEntry, newEntry]);
-        setUser({ ...user, email: "", newpassword: "" });
-        setTimeout(() => {
-            history.push("/OTP");
-        }, 1000)
+            setUser({ ...user,email: "", newpassword: "" });
+                window.alert("Submission Successfull");
+                setTimeout(() => {
+                    history.push("/OTP");
+                }, 1000)
+        }
+},[userError])
+    const submitForm = async (event) => {
+        event.preventDefault();
+        setUserError(Validate(user));
+        setIsSubmit(true);
+
+    }
+    
+    const Validate = (values)=>{
+        const error={}
+        const regexMail=/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
+        const regexPass=/^[a-zA-Z0-9@#!$%^_]{8,12}$/;
+        if(!values.email){
+            error.email="**Email Is Required!";
+        }else if(!regexMail.test(values.email)){
+            error.email="**This is not a valid Email format!";
+        }
+        if(!values.newpassword){
+            error.newpassword="**Password Is Required!";
+        }
+        else if(values.newpassword.length < 8){
+            error.newpassword="**Password must be more than 8 characters!";
+        }else if(values.newpassword.length > 12){
+            error.newpassword="**Password must be less than 12 characters!";
+        }else if(!regexPass.test(values.password)){
+            error.newpassword="**This is not a valid password!";
+        }
+
+        return error;
 
     }
     return (
@@ -43,18 +75,20 @@ function Forgot() {
 
                                 <div className="col-lg-2">
                                     <i className="fa fa-envelope icon"></i>
-                                    <input type="email" name="email" placeholder="Email" value={user.email} onChange={handleInputs} className="input1" size="30" required />
+                                    <input type="email" name="email" placeholder="Email" value={user.email} onChange={handleInputs} className="input1" size="30" />
                                 </div>
 
                             </div>
+                            <p className="required">{userError.email}</p>
                             <div className="form-row">
 
                                 <div className="col-lg-2">
                                     <i className="fa fa-lock icon"></i>
-                                    <input type="password" placeholder="New Password" name="newpassword" value={user.newpassword} onChange={handleInputs} className="input1" size="30" required />
+                                    <input type="password" placeholder="New Password" name="newpassword" value={user.newpassword} onChange={handleInputs} className="input1" size="30" />
                                 </div>
 
                             </div>
+                            <p className="required">{userError.newpassword}</p>
 
                             <div className="form-row">
 
